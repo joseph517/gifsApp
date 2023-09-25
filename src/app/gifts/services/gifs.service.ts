0e477021b34d70d88084e89808e5f7df7e8075ff
Alvaro vergara <avergara@lsv-tech.com>
2023-09-25 13:10:57 -0500

    save history in localstorage

--- src/app/gifts/services/gifs.service.ts
+++ src/app/gifts/services/gifs.service.ts
@@ -16,10 +16,22 @@ export class GifsService {
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+    this.loadLocalStorage()
+   }
 
   get tagHistory(){
     return [...this._tagHistory]
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history', JSON.stringify(this._tagHistory ))
+  }
+
+  private loadLocalStorage():void{
+    if(!localStorage.getItem('history')) return
+     this._tagHistory = JSON.parse(localStorage.getItem('history')!);
+     this.searchTag(this._tagHistory[0])
   }
 
   private organizeHistory(tag:string){
@@ -31,7 +43,11 @@ export class GifsService {
     }
     this._tagHistory.unshift(tag)
     this._tagHistory = this._tagHistory.splice(0, 10)
+    this.saveLocalStorage()
+  }
 
+  getItemLocalStorage(){
+    return localStorage.getItem('history')
   }
 
   searchTag(tag:string):void{
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -9,13 +9,13 @@ import { GifsService } from 'src/app/gifts/services/gifs.service';
 export class SidebarComponent {
   constructor(
     private gifsService: GifsService
-  ){}
+  ) { }
 
-  get tags(){
+  get tags() {
     return this.gifsService.tagHistory
   }
 
-  searchTag(tag:string):void{
+  searchTag(tag: string): void {
     this.gifsService.searchTag(tag)
   }
 
